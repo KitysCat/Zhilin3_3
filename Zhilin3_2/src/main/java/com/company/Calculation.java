39dcdf20e26334a8319507b3cb05f329fd95853d
KitysCat <86959986+KitysCat@users.noreply.github.com>
2021-07-07 15:09:52 +0300

    Add files via upload

--- Zhilin3_2/src/main/java/com/company/Calculation.java
+++ Zhilin3_2/src/main/java/com/company/Calculation.java
@@ -2,27 +2,18 @@ package com.company;
 
 public class Calculation {
     public double D;
-    public double x1, x2, x;
-    public void calculation(double a, double b, double c) {
+    double arr[] = new double[2];
+    public double[] calculation(double a, double b, double c) {
         D = b * b - 4 * a * c;
         if (D > 0) {
             double d = Math.sqrt(D);
-            x1 = (-b-d)/(2 * a);
-            x2 = (-b+d)/(2 * a);
-            //System.out.println(x1 + " " + x2);
+            arr[0] = (-b-d)/(2 * a);
+            arr[1] = (-b+d)/(2 * a);
+            return arr;
+        } else if (D == 0) {
+            arr[0] = (-b)/(2 * a);
+            return arr;
         }
-        if (D == 0) {
-            x = (-b)/(2 * a);
-            //System.out.println(x);
-        }
-    }
-    public double getX1() {
-        return x1;
-    }
-    public double getX2() {
-        return x2;
-    }
-    public double getX() {
-        return x;
+        return null;
     }
 }
--- Zhilin3_2/test/com/company/CalculatorTest.java
+++ Zhilin3_2/test/com/company/CalculatorTest.java
@@ -4,12 +4,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
     @Test
-    void cTest() {
-        Calculator calculator = new Calculator();
-        assertEquals(calculator.sum(2, 2), 4);
-        assertEquals(calculator.sub(6, 2), 4);
-        assertEquals(calculator.mul(6, 2), 12);
-        assertEquals(calculator.div(6, 2), 3);
+    void cTestSum() {
+        double sum = calculator.sum(2, 2);
+        assertEquals(4, sum);
+    }
+    @Test
+    void cTestSub() {
+        double sub = calculator.sub(6, 2);
+        assertEquals(4, sub);
+    }
+    @Test
+    void cTestMul() {
+        double mul = calculator.mul(6, 2);
+        assertEquals(12, mul);
+    }
+    @Test
+    void cTestDiv() {
+        double div = calculator.div(6, 2);
+        assertEquals(3, div);
+    }
+    @Test
+    void cTestDivZero() {
+        try {
+            double div = calculator.div(21, 0);
+            if (div == calculator.div(1, 0)) {
+                throw new  ArithmeticException("Деление на ноль");
+            }
+        } catch (ArithmeticException e) {
+            System.out.print("Деление на ноль");
+        }
     }
 }
